d888bee03af8e2d18d4d4c0bc02f316489f77cc7
Andrey.Denisov <Andrey.Denisov@tularegion.ru>
2023-09-04 17:54:20 +0300

    Ep.13 - Partials

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
-<?php
-$books = [
-    
-    [
-        'name' => 'Do Android Dreams of Electric Ship',
-        'author'=> 'Philip K. Dick',
-        'purchase_url' => 'http://example.com',
-        'year' => 1982
-    ],
-    [
-        'name' => "Project Hail Mary",
-        'author' => 'Andy Wair',
-        'purchase_url' => 'http://example.com',
-        'year' => 2021
-    ],
-    [
-        'name' => "The Martian",
-        'author' => 'Andy Wair',
-        'purchase_url' => 'http://example.com',
-        'year' => 2011
-    ],
-    
-];
- 
-$filteredBooks = array_filter($books,function($book){
-    return $book['year'] >= 1950 && $book['year'] <=2020;
-});
+<?php 
 
-require "index.view.php";
+$heading = "Home";
+
+require "views/index.view.php";
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="ru" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo Laracasts</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="h-full">
- 
-<div class="min-h-full">
-  <nav class="bg-gray-800">
+<nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
@@ -18,9 +7,9 @@
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
-              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->              
-              <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</a>
-              <a href="/about.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
               <a href="/contacts.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contacts</a>              
             </div>
           </div>
@@ -86,11 +75,9 @@
     <div class="md:hidden" id="mobile-menu">
       <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Reports</a>
+        <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+        <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"> About</a>              
+        <a href="/contacts.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contacts</a>              
       </div>
       <div class="border-t border-gray-700 pb-3 pt-4">
         <div class="flex items-center px-5">
@@ -117,21 +104,3 @@
       </div>
     </div>
   </nav>
-
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">About us</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <p>
-        This About page
-      </p>
-    </div>
-  </main>
-</div>
-
- 
-</body>
-</html>
