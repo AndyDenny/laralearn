204ac49ec1ebee2dd6c9ab3c37f24b6822497b84
AndyDenny <tp0yah@gmail.com>
2023-09-05 23:53:03 +0300

    Ep. 14 - (something wrong)

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
 require "functions.php";
 
-$heading = "Home";
-
-require "views/index.view.php";
+$uri = $_SERVER["REQUEST_URI"]; 
+ 
+dd($_SERVER);
+if ($uri == '/about'){
+    require 'controllers/about.php';
+}else if($uri == '/'){
+    require 'controllers/index.php';
+}else if($uri == '/contacts'){
+    require 'controllers/contacts.php';
+}
