805916d1ab5147db13adf95f1a4ba05ec30ac1dc
AndyDenny <tp0yah@gmail.com>
2023-09-10 23:07:28 +0300

    Ep.17 - PDO First Steps

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
  
 require "functions.php";
 
-require "router.php";
+//require "router.php";
 
- 
+$connct = "mysql:host=localhost;port=3306;dbname=laralearndb;user=root;charset=utf8mb4"; // undrescore sign - return EXSP
+$pdo = new PDO($connct);
+$statement =  $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post){
+    echo "<li>{$post['title']}</li>";
+}
