2974be8172e2e70031bc8978d20766263fb12dd1
AndyDenny <tp0yah@gmail.com>
2023-09-12 23:26:39 +0300

    Ep.20 - SQL Injection Vulnerabilities Explained

--- index.php
+++ index.php
@@ -7,8 +7,12 @@ $config = require ("config.php");
 
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+$query = "select * from posts where id = ?";
 
+$posts = $db->query($query, [$id])->fetch();
+
+dd($posts);
 
 foreach ($posts as $post){
     echo "<li>{$post['title']}</li>";
