589b7b134cc0f3f0c6b66e0ed32de2df0ef36ce0
AndyDenny <tp0yah@gmail.com>
2023-09-18 23:02:32 +0300

    Ep.27 - Introduction to Form Validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,10 +7,23 @@ $db = new Database($config['database']);
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    $errors = [];
+
+    if (strlen($_POST['body']) == 0){
+        $errors['body'] = 'A text required.';
+    }
+    
+    if (strlen($_POST['body']) >= 1000){
+        $errors['body'] = 'Too many letters, no more than 1000.';
+    }
+
+   if(empty($errors)){
     $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)',[
         'body' => htmlspecialchars($_POST['body']),
         'user_id' => 1
     ]);
+   }
 }
 
 require "views/note-create.view.php";
